8f807befb839ef05b81e31b1c12c0430d448cbf7
Michael Marsh <mmarsh@cs.umd.edu>
2020-01-20 12:14:19 -0500

    initial project commit
    There's probably some more cleanup, and I'd like to turn the
    project writeup into a markdown README

---
+++ src/pqueue/priorityqueues/MinHeapPriorityQueue.java
@@ -0,0 +1,83 @@
+package pqueue.priorityqueues; // ******* <---  DO NOT ERASE THIS LINE!!!! *******
+
+
+/* *****************************************************************************************
+ * THE FOLLOWING IMPORTS WILL BE NEEDED BY YOUR CODE, BECAUSE WE REQUIRE THAT YOU USE
+ * ANY ONE OF YOUR EXISTING MINHEAP IMPLEMENTATIONS TO IMPLEMENT THIS CLASS. TO ACCESS
+ * YOUR MINHEAP'S METHODS YOU NEED THEIR SIGNATURES, WHICH ARE DECLARED IN THE MINHEAP
+ * INTERFACE. ALSO, SINCE THE PRIORITYQUEUE INTERFACE THAT YOU EXTEND IS ITERABLE, THE IMPORT OF ITERATOR
+ * IS NEEDED IN ORDER TO MAKE YOUR CODE COMPILABLE. THE IMPLEMENTATIONS OF CHECKED EXCEPTIONS
+ * ARE ALSO MADE VISIBLE BY VIRTUE OF THESE IMPORTS.
+ ** ********************************************************************************* */
+
+import pqueue.exceptions.*;
+import pqueue.heaps.MinHeap;
+
+import java.util.Iterator;
+/**
+ * <p>{@link MinHeapPriorityQueue} is a {@link PriorityQueue} implemented using a {@link MinHeap}.</p>
+ *
+ * <p>You  <b>must</b> implement the methods of this class! To receive <b>any credit</b> for the unit tests
+ * related to this class, your implementation <b>must</b> use <b>whichever</b> {@link MinHeap} implementation
+ * among the two that you should have implemented you choose!</p>
+ *
+ * @author  ---- YOUR NAME HERE ----
+ *
+ * @param <T> The Type held by the container.
+ *
+ * @see LinearPriorityQueue
+ * @see MinHeap
+ * @see PriorityQueue
+ */
+public class MinHeapPriorityQueue<T> implements PriorityQueue<T>{
+
+	/* ***********************************************************************************
+	 * Write any private data elements or private methods for MinHeapPriorityQueue here...*
+	 * ***********************************************************************************/
+
+
+
+
+
+	/* *********************************************************************************************************
+	 * Implement the following public methods. You should erase the throwings of UnimplementedMethodExceptions.*
+	 ***********************************************************************************************************/
+		/**
+	 * Simple default constructor.
+	 */
+	public MinHeapPriorityQueue(){
+		throw new UnimplementedMethodException();
+	}
+
+	@Override
+	public void enqueue(T element, int priority) throws InvalidPriorityException {	// DO *NOT* ERASE THE "THROWS" DECLARATION!
+		throw new UnimplementedMethodException();
+	}
+
+	@Override
+	public T dequeue() throws EmptyPriorityQueueException {		// DO *NOT* ERASE THE "THROWS" DECLARATION!
+		throw new UnimplementedMethodException();
+	}
+
+	@Override
+	public T getFirst() throws EmptyPriorityQueueException {	// DO *NOT* ERASE THE "THROWS" DECLARATION!
+		throw new UnimplementedMethodException();
+	}
+
+	@Override
+	public int size() {
+		throw new UnimplementedMethodException();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		throw new UnimplementedMethodException();
+	}
+
+
+	@Override
+	public Iterator<T> iterator() {
+		throw new UnimplementedMethodException();
+	}
+
+}
